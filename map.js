25d06790da4ffee97e27b04580ba0a46c3f69a3f
abelChauwin <abel.chauwin@outlook.fr>
2025-10-18 19:27:20 +0200

    Update map.js

--- map.js
+++ map.js
@@ -13,7 +13,28 @@ const data = {
 	paris: {
 		coo: [48.856614, 2.3522219],
 		titre: "Paris",
-		president: "Nom Prenom"
+		president: { nom:"Nom Prenom",
+			courriel: "email",
+			tel: "36303630"
+			},
+		bureau: [{
+			role: "vice-presidente",
+			nom:"Monique"	
+			},
+			{
+				role: "tresorerier",
+				nom:"huber"	
+			}
+		],
+		sectionLocal: [
+			"Château-Thierry (Christiane Krabal)",
+			"Chauny",
+			"Crécy sur Serre",
+			"Laon (Catherine Religieux)",
+			"Le Nouvion en Thiérache (en sommeil)",
+			"Saint-Quentin",
+			"Soissons (Monique Vigues)"
+		],
 	}
 }
 
@@ -25,9 +46,36 @@ for (let key in data) {
 	title: agence.titre,
 	}).addTo(map);
 	
-	marker.bindPopup('<span class="popup" onclick="afficherAgence('.concat(agence.titre,')">', agence.titre,'</span>'));
+	marker.bindPopup(`
+    <span class="popup" style="cursor:pointer; color:blue;" onclick="afficherAgence('${agence.titre.replace(/'/g, "\\'")}')">
+        ${agence.titre}
+    </span>
+	`);
+
 };
 
-function afficherAgence(agence){
-	//afficher les info de l'agence
+function afficherAgence(titreAgence){
+	let agence = null;
+	for (let key in data) {
+		agence= data[key];
+		if (agence.titre==titreAgence) break;
+	}
+	let container = document.getElementById("presidence");
+	container.innerHTML = agence.president.nom
+
+	container = document.getElementById("courriel");
+	container.innerHTML = agence.president.courriel
+
+	container = document.getElementById("tel");
+	container.innerHTML = agence.president.tel
+
+	container = document.getElementById("Bureau");
+	for (let i in agence.bureau){
+		container.innerHTML +=`<p><strong class="label">${agence.bureau[i].role} : </strong> <span>${agence.bureau[i].nom}</span></p>`
+	}
+
+	container = document.getElementById("Section local");
+	for (let i in agence.sectionLocal){
+		container.innerHTML +=`<p>${agence.sectionLocal[i]}</p>`
+	}
 }
