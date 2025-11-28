192d394b8e213a8c7548d6ebe9a0f2d1e2571f27
Seppxku <hideur.samy@gmail.com>
2025-11-28 19:47:03 +0100

    fix(ui): align page title on mobile devices

--- index.php
+++ index.php
@@ -16,74 +16,7 @@
     <title>VMEH</title>
 </head>
 <body>
-
-<header>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="./index.php">
-                <img src="assets/image/navbar/VMEH_Logo.webp"
-                     alt="Main verte qui soutient un coeur avec deux personnes à l'intérieur qui semblent s'entrainer.
-                     Il y a en dessous écrit VMEH en noir suivi de la description de l'acronyme"
-                     height="100"
-                     width="150">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            L'ASSOCIATION
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="pages/histoire.php">HISTOIRE</a></li>
-                            <li><a class="dropdown-item" href="#">MISSIONS ET VALEURS</a></li>
-                            <li><a class="dropdown-item" href="#">NOTRE ORGANISATION</a></li>
-                            <li><a class="dropdown-item" href="pages/association-departementale.php">NOS ASSOCIATIONS DÉPARTEMENTALES</a></li>
-                            <li><a class="dropdown-item" href="#">NOS PARTENAIRES</a></li>
-                            <li><a class="dropdown-item" href="#">NOTRE DOCUMENTATION</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="pages/actualite.php" role="button" >
-                            ACTUALITÉS
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="pages/devenir-benevole.php">DEVENIR BÉNÉVOLE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">NOS ACTIONS</a>
-                    </li>
-
-                </ul>
-                <div class="d-flex align-items-center justify-content-end gap-3 ms-auto flex-wrap ">
-                    <a class="btn rounded-pill d-flex align-items-center text-start gap-2 px-3 px-lg-4 py-2" id="espace-membre" href="#">
-                        <img src="assets/image/navbar/photo-profil-utiisateur.webp" alt="">Espace <br>membre
-                    </a>
-                    <a class="btn rounded-pill d-flex align-items-center text-start gap-2 text-white px-3 px-lg-4 py-2" id="faire-un-don" href="#">
-                        <img src="assets/image/navbar/White-Heart-Emoji-PNG.webp"
-                             alt=""
-                             height="30"
-                             width="30"
-                        >Faire un <br>don
-                    </a>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <div class="d-flex justify-content-end">
-        <img src="assets/image/navbar/ducoeur.webp"
-             alt="Du coeur et beaucoup d'écoute écrit sur un fil noir qui se déroule d'un coeur vibrant de couleurs rouges, jaunes et oranges"
-             class="img-fluid"
-        >
-    </div>
-
-</header>
-
+<?php require_once "pages/header.php" ?>
 
 <main>
     <article>
