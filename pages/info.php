c61e5bbba65ac8d6e3d8365b99c4d093f57c7373
krabi <kunborremi.hing@gmail.com>
2025-12-07 02:10:33 +0100

    Debut de la refonte de la page actualite

---
+++ pages/info.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>VMEH - actualite</title>
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"
+    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js" defer></script>
+    <script src="../assets/js/script.js" defer></script>
+
+</head>
+
+<body>
+<?php require_once "./header.php"; ?>
+<main id="actualite-page">
+    <article>
+        <div class="info-region ">
+            <h1>VMEH en Region</h1>
+            <div class="info-carousel swiper">
+                <ul class="card-list swiper-wrapper">
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/region/Arcachon_Nord.webp" class="card-img" alt="">
+                            <h3>Santa Monica</h3>
+                            <p class="info-text">La Directrice de l'Ehpad Santa Monica, Mme L'Hermite, nous a réservé un accueil particulièrement chaleureux et instructif lors de notre visite à la section très dynamique de Civray.</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/region/Libourne.webp" class="card-img" alt="">
+                            <h3>Libourne: une section locale très active</h3>
+                            <p class="info-text">Placée sous l’entière responsabilité d’Yveline Bouetz, la section locale de Libourne est l’exemple même d’une entité qui sait à travers les années transmettre les valeurs d’humanité de notre mouvement.</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/region/Santa_Monica.webp" class="card-img" alt="">
+                            <h3>Sur le Bassin d’Arcachon Nord</h3>
+                            <p class="info-text">José Patanchon, « patriarche » bienveillant essaime les valeurs de la VMEH sur ce secteur.</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/region/Vmeh_86.webp" class="card-img" alt="">
+                            <h3>VMEH 86 : A la découverte de nos bénévoles</h3>
+                            <p class="info-text">Viviane et Jean interviennent au Logis du Val de Boivre situé à Vouneuil sous Biard, charmant petit village en périphérie de Poitiers.</p>
+                        </a>
+                    </li>
+
+                </ul>
+                <div class="swiper-button swiper-button-prev"></div>
+                <div class="swiper-button swiper-button-next"></div>
+            </div>
+        </div>
+    </article>
+    <article>
+        <div class="info-region ">
+            <h1>VMEH en Presse</h1>
+            <div class="info-carousel swiper">
+                <ul class="card-list swiper-wrapper">
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/presse/vmeh86.webp" class="card-img" alt="">
+                            <h3>LA PRESSE LOCALE SOUTIENT LA VMEH 86</h3>
+
+                            <p class="info-text">Ils sont visiteurs des malades en établissements hospitaliers
+                                et des résidents en Ehpad. Chaque semaine, ces bénévoles offrent quelques heures de leur temps dans un geste de fraternité</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/presse/La_Vmeh.webp" class="card-img" alt="">
+                            <h3>La VMEH : de la chaleur et du réconfort</h3>
+                            <p class="info-text">Découvrez l’article captivant sur la VMEH dans le magazine Seniors Actuels de décembre 2023 !</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/presse/Residence.webp" class="card-img" alt="">
+                            <h3>La résidence La Houssaie de la Fondation Partage et Vie sensibilise au bénévolat</h3>
+                            <p class="info-text">VMEH 77, comme de nombreuses autres
+                                associations départementales de la Fédération Nationale VMEH,
+                                est présente dans les deux établissements Partage et Vie de la Seine et Marne (77).</p>
+                        </a>
+                    </li>
+                    <li class="card-item swiper-slide">
+                        <a href="#" class="card-link">
+                            <img src="../assets/image/carrousel/presse/VMEH33_Jose-Patanchon.webp" class="card-img" alt="">
+                            <h3>Sur le Bassin d’Arcachon Nord</h3>
+                            <p class="info-text">Un patriarche bienveillant essaime les valeurs de la VMEH sur le Bassin Nord d’Arcachon et donne toute liberté à ses bénévoles d’enrichir leur bénévolat</p>
+                        </a>
+                    </li>
+
+                </ul>
+                <div class="swiper-button swiper-button-prev"></div>
+                <div class="swiper-button swiper-button-next"></div>
+            </div>
+        </div>
+    </article>
+
+
+    <article>
+        <h1>VMEH en Video</h1>
+        <section class="info-video">
+
+            <div class="info-video-item">
+                <a href="..">
+                    <img src="../assets/image/carrousel/video/engagement-benevole_intro.webp" alt="image d'engagement">
+                    <div class="info-video-text">
+                        <p>VMEH : Film sur l’engagement bénévole</p>
+                    </div>
+                </a>
+
+            </div>
+            <div class="info-video-item">
+                <a>
+                    <img src="../assets/image/carrousel/video/Journee-experience-patient.webp" alt="image d'engagement">
+                    <div class="info-video-text">
+                        <p>VMEH était présente à la Journée de l’Expérience Patient 2024</p>
+                    </div>
+                </a>
+
+            </div><div class="info-video-item">
+                <a>
+                    <img src="../assets/image/carrousel/video/voeux_2024_intro.webp" alt="image d'engagement">
+                    <div class="info-video-text">
+                        <p>Vœux 2024 de la VMEH</p>
+                    </div>
+                </a>
+            </div>
+        </section>
+    </article>
+
+
+</main>
+
+<?php require_once "./footer.php"; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
